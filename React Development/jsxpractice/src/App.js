54f177d57f02519c1aba11937dc9bc303ad39898
Colon Dominguez, Anthony <anthonyjomar441@gmail.com>
2025-06-01 18:32:46 -0400

    React.js beginning

---
+++ React Development/jsxpractice/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to My React App</h1>
+      <ul>
+        <li>First list item</li>
+        <li>Second list item</li>
+        <li>Third list item</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
